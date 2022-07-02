846c3eb94d75d96d40dd877878a151019f04a2ed
ophnsjh0 <61132132+ophnsjh0@users.noreply.github.com>
2022-07-02 09:49:21 +0900

    commit_0702

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,41 +1,50 @@
 import {useState,  useEffect} from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => {
-    setToDo(event.target.value);
-  }
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === "") {
-      return;
-    }
-    setToDos((current) => [toDo, ...current]);
-    setToDo("");  
-  };
-  useEffect(()=> {
-    console.log(toDos)
-  }, [toDos])
-  return( 
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [money, setMoney] = useState(0);
+  const [coinValue, setCoinValue] = useState(0);
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => 
+      setCoins(json),
+      setLoading(false)
+    )
+  }, [])
+   const onChange = (event) => {
+    setMoney(event.target.value);
+   }
+   const onChangeCoin = (event) => {
+    setCoinValue(event.target.value);
+    console.log(event.target.value);
+   }
+    
+  return(
     <div>
-      <h1> MY To Dos : {toDos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input value={toDo} 
-          placeholder="add to do" 
-          type="text" 
-          onChange={onChange} />
-        <button>Add to do</button>
-      </form>
-      <hr/>
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <h1>Coins Data : {loading ? null : coins.length} </h1>
+      {loading ? <strong>loading....</strong> : null}
+      <strong> Money : </strong>
+      $ <input value={money} placeholder="your Money plz.." type="number" onChange={onChange} ></input> USD
+      <div>
+        <select onChange={onChangeCoin}>
+          {coins.map((coin, id) => 
+          <option key={id} value={coin.quotes.USD.price}>
+            {coin.id}({coin.symbol}) : $ {coin.quotes.USD.price} USD
+          </option>)         
+          }
+        </select>
+
+        <h3>  You can buy: {Math.floor(money/coinValue)} </h3>
+         
+
+        
+      </div>
+      
+     
     </div>
-   
-  );
+  )
 }
 
 export default App;
